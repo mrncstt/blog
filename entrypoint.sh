72584006648a9c658af01ffbef49fdefd20abba9
cybi <jens@koetterheinri.ch>
2020-02-13 01:28:42 +0100

    fix issue with a failing build when there is nothing to commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,10 +40,7 @@ touch .nojekyll
 echo "Add all files."
 git add -A -v
 git status
-echo "Commit changes."
-git commit -m 'Hugo build from Action'
-echo "Push."
-git push origin
+git diff-index --quiet HEAD || echo "Commit changes." && git commit -m 'Hugo build from Action' && echo "Push." && git push origin
 
 echo "#################################################"
 echo "Published"
